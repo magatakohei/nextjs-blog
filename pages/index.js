05b79a5341eb799d2231ce248024abceb6207a6b
magako <qutianh@gmail.com>
2021-02-17 10:53:41 +0900

    add tailwind css

--- pages/index.js
+++ pages/index.js
@@ -1,9 +1,8 @@
-import Head from 'next/head'
-import Layout, { siteTitle } from '../components/layout'
-import utilStyles from '../styles/utils.module.css'
-import { getSortedPostsData } from '../lib/posts'
-import Link from 'next/link'
-import Date from '../components/date'
+import Head from "next/head";
+import Layout, { siteTitle } from "../components/layout";
+import { getSortedPostsData } from "../lib/posts";
+import Link from "next/link";
+import Date from "../components/date";
 
 export default function Home({ allPostsData }) {
   return (
@@ -11,38 +10,42 @@ export default function Home({ allPostsData }) {
       <Head>
         <title>{siteTitle}</title>
       </Head>
-      <section className={utilStyles.headingS}>
-        <p>こんにちは！magakoのブログです。</p>
-        <p>都内の会社で社内SEとして業務をしています！</p>
-        <p>Webアプリケーション開発や開発手法など調べたことを発信していきます。</p>
-        <p>下記の記事もみていただけると嬉しいです！！</p>
-
+      <section className="max-w-lg mx-auto flex justify-center text-lg">
+        <p>
+          こんにちは！magakoのブログです。
+          <br />
+          都内の会社で社内SEとして業務をしています！
+          <br />
+          下記の記事もみていただけると嬉しいです！！
+        </p>
       </section>
-      <section className={`${utilStyles.headingMd} ${utilStyles.padding1px}`}>
-        <h2 className={utilStyles.headingLg}>Blog</h2>
-        <ul className={utilStyles.list}>
-          {allPostsData.map(({ id, createdDate, title }) => (
-            <li className={utilStyles.listItem} key={id}>
-              <Link href={`/posts/${id}`}>
-                <a>{title}</a>
-              </Link>
-              <br />
-              <small className={utilStyles.lightText}>
-                <Date dateString={createdDate} />
-              </small>
-            </li>
-          ))}
-        </ul>
+      <section className="mt-5 text-lg pt-1 max-w-xl mx-auto">
+        <h2 className="text-3xl font-bold">Blog</h2>
+        <div className="flex justify-center">
+          <ul className="list-none m-0 p-0">
+            {allPostsData.map(({ id, createdDate, title }) => (
+              <li className="mb-5" key={id}>
+                <Link href={`/posts/${id}`}>
+                  <a className="text-blue-500 hover:text-blue-600">{title}</a>
+                </Link>
+                <br />
+                <small className="text-gray-400">
+                  <Date dateString={createdDate} />
+                </small>
+              </li>
+            ))}
+          </ul>
+        </div>
       </section>
     </Layout>
-  )
+  );
 }
 
 export async function getStaticProps() {
   const allPostsData = getSortedPostsData();
   return {
     props: {
-      allPostsData
-    }
-  }
+      allPostsData,
+    },
+  };
 }
--- pages/posts/[id].js
+++ pages/posts/[id].js
@@ -1,16 +1,15 @@
-import Layout from '../../components/layout'
-import { getAllPostIds, getPostData } from '../../lib/posts'
-import Head from 'next/head'
-import Date from '../../components/date'
-import utilStyles from '../../styles/utils.module.css'
+import Layout from "../../components/layout";
+import { getAllPostIds, getPostData } from "../../lib/posts";
+import Head from "next/head";
+import Date from "../../components/date";
 import Markdown from "markdown-to-jsx";
 import Prism from "prismjs";
 import { useEffect } from "react";
 
 export default function Post({ postData }) {
   useEffect(() => {
-    Prism.highlightAll()
-  }, [])
+    Prism.highlightAll();
+  }, []);
 
   return (
     <Layout>
@@ -18,29 +17,31 @@ export default function Post({ postData }) {
         <title>{postData.title}</title>
       </Head>
       <article>
-        <h1 className={utilStyles.headingXl}>{postData.title}</h1>
-        <div className={utilStyles.lightText}>
+        <h1 className="text-4xl leading-snug font-extrabold tracking-tighter my-5">
+          {postData.title}
+        </h1>
+        <div className="text-gray-500">
           <Date dateString={postData.createdDate} />
         </div>
         <Markdown>{postData.contentHtml}</Markdown>
       </article>
     </Layout>
-  )
+  );
 }
 
 export async function getStaticPaths() {
-  const paths = getAllPostIds()
+  const paths = getAllPostIds();
   return {
     paths,
-    fallback: false
-  }
+    fallback: false,
+  };
 }
 
 export async function getStaticProps({ params }) {
-  const postData = await getPostData(params.id)
+  const postData = await getPostData(params.id);
   return {
     props: {
-      postData
-    }
-  }
+      postData,
+    },
+  };
 }
